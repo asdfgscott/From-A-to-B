c504f3c338b721c93d9b6d94ef2c2a3f7ca8d87b
Scott Truong <asdfgscott@gmail.com>
2021-10-06 23:02:44 -0400

    Getting inputs and making sure they make it out

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace From_A_to_B
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! something");
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int counter = 0;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(a+b);
         }
     }
 }
