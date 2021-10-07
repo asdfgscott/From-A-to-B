a8001bc83ae5a287a5684c8104df4c8d4566cc68
Scott Truong <asdfgscott@gmail.com>
2021-10-06 22:45:44 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace From_A_to_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
